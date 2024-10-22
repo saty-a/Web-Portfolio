dcd12d9fa4e5f5a892b1f468e5a79e7989bdbbdf
Satya Prakash <satyaprakash6945@gmail.com>
2024-10-22 15:26:14 +0530

    Add: home view

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../../../widgets/responsive.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -11,23 +12,68 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.fromLTRB(100, 80, 100, 100),
-        color: Colors.black,
-        child: Align(
-            alignment: Alignment.topRight,
-            child: Container(
-              height: Get.height,
-              width: Get.width,
-              child: Text(
-                "Satya Prakash",
-                style: GoogleFonts.aboreto(
-                  textStyle: Theme.of(context).textTheme.displayLarge,
-                  fontSize: 48,
-                  fontWeight: FontWeight.w700,
-                )
-              ),
-            )),
+      body: SafeArea(
+        child: Stack(
+          children: [
+
+            Obx(() {
+              return controller.isInternet.value
+                  ? Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                        height: Get.height * .061,
+                        width: Get.width,
+                        decoration: const BoxDecoration(
+                            color: Colors.lightBlueAccent,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                topRight: Radius.circular(20))),
+                        child: Center(
+                          child: Wrap(
+                            alignment: WrapAlignment.center,
+                            children: [
+                              InkWell(
+                                onTap: () {},
+                                child: const Text(
+                                  "About",
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.white),
+                                ),
+                              ),
+                              InkWell(
+                                onTap: () {},
+                                child: const SizedBox(
+                                  width: 20,
+                                ),
+                              ),
+                              InkWell(
+                                onTap: () {},
+                                child: const Text(
+                                  "Experience",
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.white),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 20,
+                              ),
+                              InkWell(
+                                onTap: () {},
+                                child: const Text(
+                                  "Projects",
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.white),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  : const SizedBox.shrink();
+            })
+          ],
+        ),
       ),
     );
   }
