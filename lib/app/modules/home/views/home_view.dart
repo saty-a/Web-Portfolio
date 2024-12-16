ad73b8965b556cca09f1fbac9600cd3fa968cd23
Satya Prakash <satyaprakash6945@gmail.com>
2024-12-16 14:40:29 +0530

    Update: xml updates

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Container(
+        padding: const EdgeInsets.fromLTRB(100, 80, 100, 100),
+        color: Colors.black,
+        child: Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              height: Get.height,
+              width: Get.width,
+              child: Text(
+                "Satya Prakash",
+                style: GoogleFonts.aboreto(
+                  textStyle: Theme.of(context).textTheme.displayLarge,
+                  fontSize: 48,
+                  fontWeight: FontWeight.w700,
+                )
+              ),
+            )),
       ),
     );
   }
