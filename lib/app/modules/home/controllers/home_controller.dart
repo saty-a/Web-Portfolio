6b96db60d3f3b55fb5096b5b5ce477ef1c8aa2fa
Satya Prakash <satyaprakash6945@gmail.com>
2025-01-12 10:00:00 +0530

    Add HomeController with theme management and system theme sync

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,6 +1,45 @@
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
 
 class HomeController extends GetxController {
+  final RxBool isDarkMode = false.obs;
+  final RxInt currentIndex = 0.obs;
 
-  RxBool isInternet = false.obs;
+  @override
+  void onInit() {
+    super.onInit();
+    // Initialize theme based on system preference
+    final window = WidgetsBinding.instance.window;
+    isDarkMode.value = window.platformBrightness == Brightness.dark;
+    
+    // Listen to system theme changes
+    window.onPlatformBrightnessChanged = () {
+      if (!Get.isDarkMode) { // Only update if user hasn't manually set theme
+        isDarkMode.value = window.platformBrightness == Brightness.dark;
+        Get.changeThemeMode(isDarkMode.value ? ThemeMode.dark : ThemeMode.light);
+      }
+    };
+  }
+
+  void toggleTheme() {
+    isDarkMode.value = !isDarkMode.value;
+    Get.changeThemeMode(isDarkMode.value ? ThemeMode.dark : ThemeMode.light);
+  }
+
+  void setCurrentIndex(int index) {
+    currentIndex.value = index;
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+    // Called after the widget is rendered on screen
+  }
+
+  @override
+  void onClose() {
+    // Remove the listener when controller is disposed
+    WidgetsBinding.instance.window.onPlatformBrightnessChanged = null;
+    super.onClose();
+  }
 }
