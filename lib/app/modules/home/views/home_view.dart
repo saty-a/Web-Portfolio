0b90299ee3a479f55be55c0f4b2eb9e68b1ff584
Satya Prakash <satyaprakash6945@gmail.com>
2025-01-13 10:00:00 +0530

    Add responsive HomeView with theme toggle and initial layout

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:webportfolio/app/modules/about/views/about.dart';
-import '../../../routes/app_pages.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -10,126 +9,55 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(color: Colors.grey),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Align(
-              alignment: Alignment.topCenter,
-              child: Container(
-                height: Get.height * .061,
-                width: Get.width,
-                decoration: const BoxDecoration(
-                    color: Colors.black54,
-                    boxShadow: [],
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(20),
-                        bottomRight: Radius.circular(20))),
-                child: Center(
-                  child: Wrap(
-                    alignment: WrapAlignment.center,
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          Get.toNamed(Routes.ABOUT);
-                        },
-                        child: const Text(
-                          "About",
-                          style: TextStyle(fontSize: 20, color: Colors.white),
-                        ),
-                      ),
-                      InkWell(
-                        onTap: () {},
-                        child: const SizedBox(
-                          width: 20,
-                        ),
-                      ),
-                      InkWell(
-                        onTap: () {},
-                        child: const Text(
-                          "Experience",
-                          style: TextStyle(fontSize: 20, color: Colors.white),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 20,
-                      ),
-                      InkWell(
-                        onTap: () {},
-                        child: const Text(
-                          "Projects",
-                          style: TextStyle(fontSize: 20, color: Colors.white),
-                        ),
-                      )
-                    ],
-                  ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 16.h),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Header with theme toggle
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'My Portfolio',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    Obx(() => Tooltip(
+                          message: controller.isDarkMode.value
+                              ? 'Switch to Light Mode'
+                              : 'Switch to Dark Mode',
+                          child: IconButton(
+                            onPressed: controller.toggleTheme,
+                            icon: AnimatedSwitcher(
+                              duration: const Duration(milliseconds: 300),
+                              child: Icon(
+                                controller.isDarkMode.value
+                                    ? Icons.light_mode
+                                    : Icons.dark_mode,
+                                key: ValueKey(controller.isDarkMode.value),
+                              ),
+                            ),
+                          ),
+                        )),
+                  ],
                 ),
-              ),
+                SizedBox(height: 32.h),
+                // Hero section
+                Text(
+                  'Welcome to my portfolio',
+                  style: Theme.of(context).textTheme.displaySmall,
+                ),
+                SizedBox(height: 16.h),
+                Text(
+                  'I create beautiful and functional web experiences',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                // Add more sections here as we progress
+              ],
             ),
-            SizedBox(height: 20),
-            About(),
-            // Obx(() {
-            //   return controller.isInternet.value
-            //       ? Align(
-            //           alignment: Alignment.bottomCenter,
-            //           child: Container(
-            //             height: Get.height * .061,
-            //             width: Get.width,
-            //             decoration: const BoxDecoration(
-            //                 color: Colors.lightBlueAccent,
-            //                 borderRadius: BorderRadius.only(
-            //                     topLeft: Radius.circular(20),
-            //                     topRight: Radius.circular(20))),
-            //             child: Center(
-            //               child: Wrap(
-            //                 alignment: WrapAlignment.center,
-            //                 children: [
-            //                   InkWell(
-            //                     onTap: () {
-            //                       Get.toNamed(Routes.ABOUT);
-            //                       debugPrint("about");
-            //                     },
-            //                     child: const Text(
-            //                       "About",
-            //                       style: TextStyle(
-            //                           fontSize: 20, color: Colors.white),
-            //                     ),
-            //                   ),
-            //                   InkWell(
-            //                     onTap: () {},
-            //                     child: const SizedBox(
-            //                       width: 20,
-            //                     ),
-            //                   ),
-            //                   InkWell(
-            //                     onTap: () {},
-            //                     child: const Text(
-            //                       "Experience",
-            //                       style: TextStyle(
-            //                           fontSize: 20, color: Colors.white),
-            //                     ),
-            //                   ),
-            //                   const SizedBox(
-            //                     width: 20,
-            //                   ),
-            //                   InkWell(
-            //                     onTap: () {},
-            //                     child: const Text(
-            //                       "Projects",
-            //                       style: TextStyle(
-            //                           fontSize: 20, color: Colors.white),
-            //                     ),
-            //                   )
-            //                 ],
-            //               ),
-            //             ),
-            //           ),
-            //         )
-            //       : const SizedBox.shrink();
-            // })
-          ],
+          ),
         ),
       ),
     );
