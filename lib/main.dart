63d8cd19011d54ce1d24627372455f90c0e08fde
Satya Prakash <satyaprakash6945@gmail.com>
2025-01-14 10:00:00 +0530

    Setup main app with ScreenUtil and theme initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
-import 'app/routes/app_pages.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:webportfolio/app/routes/app_pages.dart';
+import 'package:webportfolio/app/core/theme/app_theme.dart';
 
 void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-      defaultTransition: Transition.fadeIn,
-      debugShowCheckedModeBanner: false,
-    ),
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final window = WidgetsBinding.instance.window;
+    final isDark = window.platformBrightness == Brightness.dark;
+
+    return ScreenUtilInit(
+      designSize: const Size(1920, 1080), // Design size for web
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return GetMaterialApp(
+          title: 'Web Portfolio',
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
+          initialRoute: AppPages.INITIAL,
+          getPages: AppPages.routes,
+          defaultTransition: Transition.fade,
+        );
+      },
+    );
+  }
 }
